219bf5871f0072ffb97760e37a491b8a46d86074
Seth <sethyuan@gmail.com>
2022-05-31 14:30:32 +0800

    feat: complete implementation

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,36 @@
 import "@logseq/libs"
 
-let locale = "en"
+const DEFAULT_LOCALE = "en"
+
+let locale = DEFAULT_LOCALE
 let translations: { [locale: string]: { [key: string]: string } } = {}
 
-export async function setup(
-  urlTemplate: string,
-  builtinTranslations?: { [locale: string]: { [key: string]: string } },
-) {
+export async function setup({
+  urlTemplate,
+  defaultLocale = DEFAULT_LOCALE,
+  builtinTranslations,
+}: {
+  urlTemplate: string
+  defaultLocale?: string
+  builtinTranslations?: { [locale: string]: { [key: string]: string } }
+}) {
   locale = (await logseq.App.getUserConfigs()).preferredLanguage
-  const url = urlTemplate.replace("${locale}", locale)
-  try {
-    const res = await fetch(url)
-    if (res.ok) {
-      const remoteTranslations = await res.json()
-      translations = {
-        ...builtinTranslations,
-        ...remoteTranslations,
+
+  if (locale === defaultLocale) return
+
+  if (builtinTranslations?.[locale] != null) {
+    translations = builtinTranslations
+  } else {
+    const url = urlTemplate.replace("${locale}", locale)
+    try {
+      const res = await fetch(url)
+      if (res.ok) {
+        const remoteTranslations = await res.json()
+        translations = { [locale]: remoteTranslations }
       }
+    } catch (err) {
+      console.error("translation fetch failed.\n", err)
     }
-  } catch (err) {
-    translations = { ...builtinTranslations }
   }
 }
 
